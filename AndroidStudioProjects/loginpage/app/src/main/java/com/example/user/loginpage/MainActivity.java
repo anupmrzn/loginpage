28ec65fa7ab772af1eea50f10de2f889ce7d3d58
Anup mj <33210236+anupmrzn@users.noreply.github.com>
2017-11-05 22:44:46 +0545

    Initial commit

---
+++ AndroidStudioProjects/loginpage/app/src/main/java/com/example/user/loginpage/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.user.loginpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    TextView textView;
+    TextView textView2;
+    TextView textView3;
+    Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        login = (Button)findViewById(R.id.login);
+        login.setOnClickListener(View );
+        textView = (TextView)findViewById(R.id.textView);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        textView3 = (TextView)findViewById(R.id,textView3);
+    }
+    public void setLogin(){
+        username = username.getText().toString().trim();
+        password = password.getText().toString().trim();
+
+    }
+}
